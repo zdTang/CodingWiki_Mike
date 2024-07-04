afe4e0f773793fd929076467e0a5d1477d440fa4
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-07-04 10:16:38 -0400

    9 - Create ApplicationDbContext and Book Model

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingWiki_Model.Models
+{
+    public class Book
+        {
+            public int IdBook { get; set; }
+            public string Title { get; set; }
+            public string ISBN { get; set; }
+            public double Price { get; set; }
+        }
+}
