d313283165278c0063920f08f665a6b138fbb66e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-07-04 10:27:45 -0400

    10 - Configuring Connection String

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -6,5 +6,9 @@ namespace CodingWiki_DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=.;Database=CondingWiki;TrustServerCertificate=True;Trusted_Connection=True");
+        }
     }
 }
