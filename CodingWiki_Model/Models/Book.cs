d6173a434ac0911ff693d165107d1f061250d701
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-07-04 10:35:00 -0400

    11 - Add Migration

--- CodingWiki_Model/Models/Book.cs
+++ CodingWiki_Model/Models/Book.cs
@@ -1,8 +1,12 @@
-﻿namespace CodingWiki_Model.Models
+﻿//using System.ComponentModel.DataAnnotations;
+
+namespace CodingWiki_Model.Models
 {
     public class Book
         {
-            public int IdBook { get; set; }
+            //[Key]                             // will specify a primary key
+            //public int Id { get; set; }       // if the field is id then it will be regarded as primary key
+            public int BookId { get; set; }     // if the field name is ended with "id",it will be used as primary key
             public string Title { get; set; }
             public string ISBN { get; set; }
             public double Price { get; set; }
